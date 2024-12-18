c692cac86b3a30bd4be9cf0f401e28ca8765387b
Sohag Hasan <mailsohaghasan@gmail.com>
2024-12-18 17:09:30 +0600

    call to action

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header/Header';
 import About from './About/About';
 import Skill from './Skill/Skill';
+import Action from './Action/Action';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header /> 
       <About />
       <Skill />
+      <Action />
       
     </div>
   );
