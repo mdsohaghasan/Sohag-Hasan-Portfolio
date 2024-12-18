4b9624beb12c9ae8933ec51911607847f1785b2a
Sohag Hasan <mailsohaghasan@gmail.com>
2024-12-18 17:25:39 +0600

    navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-900 p-6 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">
-          <a href="/">MyLogo</a>
+        <div className="text-white text-2xl font-extrabold">
+          <a href="/" className="hover:text-gray-400 transition duration-300">MyLogo</a>
         </div>
-        <ul className="flex space-x-4">
-          <li><Link href="/" className="text-gray-300 hover:text-white">Home</Link></li>
-          <li><Link href="/about" className="text-gray-300 hover:text-white">About</Link></li>
-          <li><Link href="/service" className="text-gray-300 hover:text-white">Service</Link></li>
-          <li><Link href="/projects" className="text-gray-300 hover:text-white">Projects</Link></li>
-          <li><Link href="/blog" className="text-gray-300 hover:text-white">Blog</Link></li>
-          <li><Link href="/contact" className="text-gray-300 hover:text-white">Contact</Link></li>
+        <ul className="flex space-x-8">
+          <li><Link href="/" className="text-gray-300 hover:text-white transition duration-300">Home</Link></li>
+          <li><Link href="/about" className="text-gray-300 hover:text-white transition duration-300">About</Link></li>
+          <li><Link href="/service" className="text-gray-300 hover:text-white transition duration-300">Service</Link></li>
+          <li><Link href="/projects" className="text-gray-300 hover:text-white transition duration-300">Projects</Link></li>
+          <li><Link href="/blog" className="text-gray-300 hover:text-white transition duration-300">Blog</Link></li>
+          <li><Link href="/contact" className="text-gray-300 hover:text-white transition duration-300">Contact</Link></li>
         </ul>
       </div>
     </nav>
