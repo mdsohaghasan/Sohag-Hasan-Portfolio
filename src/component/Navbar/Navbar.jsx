98d77cb4faa32ff4629a39de764de61acee0dc8c
Sohag Hasan <mailsohaghasan@gmail.com>
2024-12-19 14:19:09 +0600

    navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,32 +1,103 @@
-import Link from 'next/link';
-import React from 'react';
-import Image from 'next/image';
-import logo from '../../component/Navbar/Sohag-Hasan-Logo-White.png';
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+// import logo from "../../component/Navbar/Sohag-Hasan-Logo-White.png";
+import React, { useState } from 'react'
 
 const Navbar = () => {
-  return (
-    <nav className="bg-gray-900 p-6 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-extrabold">
-          <Link href="/" className="hover:text-gray-400 transition duration-300">
-            <Image src={logo} alt="Logo" width={100} height={100} />
-          </Link>
-        </div>
 
-        {/* menu Item  */}
-        <div className="flex justify-end">
-          <ul className="flex space-x-5 text-xl">
-            <li><Link href="/" className="text-gray-300 hover:text-white transition duration-300">Home</Link></li>
-            <li><Link href="/about" className="text-gray-300 hover:text-white transition duration-300">About Us</Link></li>
-            <li><Link href="/work" className="text-gray-300 hover:text-white transition duration-300">Work</Link></li>
-            <li><Link href="/insights" className="text-gray-300 hover:text-white transition duration-300">Insights</Link></li>
-            <li><Link href="/blog" className="text-gray-300 hover:text-white transition duration-300">Blog</Link></li>
-            <li><Link href="/contact" className="text-gray-300 hover:text-white transition duration-300">Contact</Link></li>
-          </ul>
+  const [mobileToggle, setMobileToggle] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMobileToggle(!mobileToggle);
+  }
+
+  return (
+    <header className="st-site-header st-style1 st-sticky-header">
+      <div className="st-main-header">
+        <div className="container">
+          <div className="st-main-header-in">
+            <div className="st-main-header-left">
+              <Link className="st-site-branding" href="/">
+                {/* <Image src={logo} alt="Mokhter Ahmad" /> */}
+              </Link>
+            </div>
+            <div className="st-main-header-right">
+              <div className="st-nav">
+                <ul className="st-nav-list st-onepage-nav" style={{ display: `${mobileToggle ? 'block' : 'none'}` }}>
+                  <li>
+                    <Link href="/" className="st-smooth-move" onClick={() => setMobileToggle(false)}>
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/about" className="st-smooth-move" onClick={() => setMobileToggle(false)}>
+                      About
+                    </Link>
+                  </li>
+                  {/* Submenu  */}
+                  <span className="dropdown relative st-smooth-move me-5">
+                    <li className="rounded inline-flex items-center me-1">
+                      <Link href="/lecture" className="st-smooth-move" onClick={() => setMobileToggle(false)}>
+                        MEDIA
+                      </Link>
+                    </li>
+                    <i className="bi bi-chevron-down"></i>
+                    <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                      <li className="">
+                        <Link
+                          className="st-smooth-move rounded-t bg-gray-200 hover:bg-[#FEC544] py-2 px-4 block"
+                          href="/lecture" onClick={() => setMobileToggle(false)} >
+                          LECTURES
+                        </Link>
+                      </li>
+                      <li className="">
+                        <Link
+                          className="st-smooth-move bg-gray-200 hover:bg-[#FEC544] py-2 px-4 block"
+                          href="/podcast" onClick={() => setMobileToggle(false)}>
+                          PODCAST
+                        </Link>
+                      </li>
+                      <li className="">
+                        <Link
+                          className="st-smooth-move rounded-b bg-gray-200 hover:bg-[#FEC544] py-2 px-4 block"
+                          href="/gallery" onClick={() => setMobileToggle(false)}>
+                          GALLERY
+                        </Link>
+                      </li>
+                    </ul>
+                  </span>
+                  {/* Submenu  */}
+                  <li>
+                    <Link href="/consultancy" className="st-smooth-move" onClick={() => setMobileToggle(false)}>
+                      CONSULTANCY
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/blog" className="st-smooth-move" onClick={() => setMobileToggle(false)}>
+                      Blog
+                    </Link>
+                  </li>
+                </ul>
+                <div className={`st-munu-toggle ${mobileToggle ? "st-toggle-active" : ""} `}  onClick={handleToggleMenu}>
+                  <span></span>
+                </div>
+                <div className="sp-phone">
+                  <div className="st-hero-btn">
+                    <Link
+                      href="/applyProgram"
+                      className="st-btn st-style1 st-color1 st-smooth-move"
+                    >
+                      Apply Program
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        
       </div>
-    </nav>
+    </header>
   );
 };
 
